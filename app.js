c68a028deb5112b050554a52374d77c1cdd774e1
c0braBubbles <233566@usn.no>
2022-01-14 16:21:43 +0100

    Første commit! Oppsett av node js komponenter, filer og filhierarki

---
+++ app.js
@@ -0,0 +1,21 @@
+// Imports
+var path = require('path'); 
+var express = require('express'); 
+
+
+var app = express();
+app.set('view engine', 'ejs'); 
+app.set('views', path.resolve(__dirname, 'views')); 
+
+
+/* Henting av sidene når brukeren forespør en av de tre hovedsidene */
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+
+// Gjør mappen public synlig, så app-en kan ta bruk av:
+app.use(express.static(path.resolve(__dirname, 'public')));
+//Avgjør hvilken port programmet skal "lytte" på:
+app.listen(process.env.PORT || 3000);
+console.log("Lytter på port 3000");
