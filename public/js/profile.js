d6b50dc5dd637f83d63c0761e8f2a7455cbcb586
🌝Jacob🌚 <Jacobkristensen@hotmail.no>
2022-02-21 22:40:15 +0100

    funksjonalitet for å legge til biografi

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,8 @@
+var uid
+
 function lagreBrukerEndringer() {
-    var uid = firebase.auth().currentUser.uid;
-    var inpBio = document.getElementById("bio_textfield");
+    uid = firebase.auth().currentUser.uid;
+    var inpBio = document.getElementById("bioTextfield");
     const con = firebase.database().ref('Bruker').child(uid);
     con.update({
         "Biografi":inpBio.value
@@ -9,3 +11,24 @@ function lagreBrukerEndringer() {
     });
 }
 
+var b_bio = document.getElementById("userBio");
+var biografi;
+
+firebase.auth().onAuthStateChanged((user) => {
+	if (user != null) {
+		uid = user.uid;
+		//console.log(uid);
+
+		var userId = firebase.auth().currentUser.uid;
+		return firebase.database().ref('/Bruker/' + uid).once('value').then((snapshot) => {
+			biografi = snapshot.child("Biografi").val();
+			document.getElementById("userBio").innerHTML = biografi;
+		}).then(() => { localStorage.setItem("Biografi", biografi); }); //Legger biografien inn i localstorage
+
+
+	} else {
+		// User is signed out
+		console.log("ingen bruker er innlogget")
+		window.location = "/";
+	}
+});
