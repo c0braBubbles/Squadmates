4b2919f441407e3dbc3bf19917a2778c8db4d3d0
c0braBubbles <233566@usn.no>
2022-04-12 01:13:48 +0200

    fremdriftslagring

--- public/js/action.js
+++ public/js/action.js
@@ -3,6 +3,7 @@ document.getElementById("btnDDown").onclick = function () {
 }
 
 const whiz = JSON.parse(sessionStorage.getItem("bruker"));
+let spill = [];
 
 
 function showProfile(name, uid2) {
@@ -30,11 +31,10 @@ function showProfile(name, uid2) {
 
 function gameSearch(i) {
     // console.log(i);
-    let inp = document.getElementById("input").value.toUpperCase();
-    let para = document.getElementById("demo");
+    let inp = document.getElementById("gameInput").value.toUpperCase();
+    let resultList = document.getElementById("gameResults"); 
 
-    if (i == 4) {
-        console.log("ferdig");
+    if (i == 10) {
         return;
     } else {
         fetch("https://api.rawg.io/api/games?key=04fe2e746e1346738846b6ea6554a96b" + "&page=" + i, {
@@ -42,21 +42,42 @@ function gameSearch(i) {
         }).then(response => response.json()).then(data => {
             for (let j = 0; j < data.results.length; j++) {
                 let game = data.results[j].name;
+                let image = data.results[j].background_image;
                 // para.innerHTML += game + "<br>";
 
                 if (game.toUpperCase().indexOf(inp) > -1) {
-                    para.innerHTML += game + "<br>";
+                    resultList.innerHTML += `<div class="input-group mx-3 w-75 d-flex mx-auto">
+                                                <div class="input-group-text bg-dark border-dark">
+                                                    <input class="form-check-input mt-0" id="st${game}" type="checkbox" onclick="checkGame('${game}', '${image}', this.id)" value="${game}" aria-label="@Gamertag">
+                                                </div>
+                                                <img src="${image}" alt="spillbilde" width="42">
+                                                <input type="text" class="form-control bg-dark border-dark text-light" aria-label="@Gamertag" value="${game}" readonly><br>
+                                            </div><br>`;
                 }
 
                 if (j == data.results.length - 1) {
                     i += 1;
-                    search(i);
+                    gameSearch(i);
                 }
             }
         }).catch(err => {
-            para.innerHTML = err;
+            console.log(err);
+            // para.innerHTML = err;
             // return;
         });
+    }
+}
+
+
+function checkGame(game, image, id) {
+    let box = document.getElementById(id); 
+
+    if(box.checked == true) {
+        spill.push(game); 
+        sessionStorage.setItem("spill", spill);
+    } else {
+        spill.splice(spill.indexOf('game'), 1);
+        sessionStorage.setItem("spill", spill);
     }
 }
 
